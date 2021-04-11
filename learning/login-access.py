72e21dc7b50c3993634cf040491daad4d2616054
Diego Montejano <diegomontejano@icloud.com>
2021-04-10 21:39:48 -0300

    v1

---
+++ learning/login-access.py
@@ -0,0 +1,8 @@
+while True:
+    username = input("Type your username: ")
+    if (username != "Diego"):
+        continue
+    password = input("Type your password: ")
+    if (password == "pass21"):
+        break
+print("Welcome! Access Allowed!")
