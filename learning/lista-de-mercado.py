577474f11ed51bfc280e6d368302f660e4b5b6b7
Diego Montejano <diegomontejano@icloud.com>
2021-04-19 21:43:04 -0300

    v1

--- learning/lista-de-mercado.py
+++ learning/lista-de-mercado.py
@@ -5,6 +5,6 @@ for i in range(3):
     item.append(input("Digite o nome do item: "))
     item.append(int(input("Digite a quantidade: ")))
     item.append(float(input("Digite o valor: ")))
-    mercado.append(item[:]) #  Usamos [:] para alterarmos os valores deste "item", mentendo os valores do "item" original
+    mercado.append(item[:]) #  Usamos [:] para alterarmos os valores deste "item", mantendo os valores do "item" original
     item.clear()
 print(mercado) 
