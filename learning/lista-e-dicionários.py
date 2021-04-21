b813947049e79ab3ca7aa5048c9d3fbe1c447686
Diego Montejano <diegomontejano@icloud.com>
2021-04-21 17:58:02 -0300

    v1

---
+++ learning/lista-e-dicionários.py
@@ -0,0 +1,6 @@
+games = [] # Aqui temos uma lista [] com vários dicionários {}
+game1 = {"nome": "Super Mario", "plataforma": "Nintendo", "ano": 1990}
+game2 = {"nome": "Zelda Oscarina of Time", "plataforma": "Nintendo 64", "ano": 1998}
+game3 = {"nome": "Pokemon Yellow", "plataforma": "GameBoy", "ano": 1999}
+games = [game1, game2, game3]
+print(games)
